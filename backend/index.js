345a6ee4ca5bb458e36f60a8a11f837f79b8b701
Tristan DAVID <tristandavid50@yahoo.fr>
2021-12-16 17:35:34 +0100

    Connexion table Workbench

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,33 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize('sequelize-p7', 'root', 'noobolife75z$', {
-    dialect: 'mysql'
+    dialect: 'mysql',
 })
 
-sequelize.authenticate().then(() => {
-    console.log("Connection succesful!")
-}).catch((err) => {
-    console.log("Error connecting to database!")
+const User = sequelize.define('user', {
+    user_id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false // allowNull oblige l'user à utiliser un nom
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING
+    },
+    age: {
+        type: Sequelize.DataTypes.INTEGER
+    },
+    WiitCodeRoks: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+},
+{
+    freezeTableName: true, // empêche la pluralisation de mySQL Workbench
+    timestamps: false  
 })
 
-console.log("Another task.")
+console.log(sequelize.models.user)
