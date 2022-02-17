1d6c59fb30d0b14bf2cb6b9584a9de9236c8cd64
Tristan DAVID <tristandavid50@yahoo.fr>
2022-02-17 12:45:55 +0100

    Mise en place Vue.js + logo

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p v-if="abonnes == 0">Aucun abonnés pour le moment 😥</p>
+    <p v-else-if="abonnes == 1">Une personne s'est abonnée, c'est super ! 💪</p>
+    <p v-else>{{abonnes}} personnes se sont abonnées 🔥</p>
+    <button @click="subscribe()">S'abonner !</button>
+    <div class="comment" v-for="(comment, idx) in comments" v-bind:key="idx">
+      <h3>{{comment.nom}}</h3>
+      <p>{{comment.commentaire}}</p>
+      <div v-if="comment.note > 1">
+        <span v-for="i in comment.note" v-bind:key="i">🍌</span>
+      </div>
+      <span v-else>💩</span>
+    </div>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      abonnes: 0,
+      comments: [{
+        nom: 'Tristan',
+        commentaire: "C'est de la bombe !",
+        note: 2,
+        },{
+        nom: 'Jean Michel',
+        commentaire: "J'adore ce que tu fais continue !",
+        note: 5},{
+        nom: 'Marc Aigri',
+        commentaire: "C'est nul",
+        note: 1,
+      }]
+    }
+  },
+  methods: {
+    // Cette fonction va permettre d'incrémenter le nombre d'abonnés
+    subscribe: function () {
+      this.abonnes++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+p {
+  font-size: 22px
+}
+
+</style>
